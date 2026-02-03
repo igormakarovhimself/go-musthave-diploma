7ac91ab1f420e32c5a69fe2cb5c4bf10637fca62
Igor Makarov <IgorMakarovHimself@gmail.com>
2026-02-03 21:39:57 +0300

    Add functional options

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,56 +17,123 @@ type Config struct {
 	AccrualCheckInterval  time.Duration
 }
 
-func Load() (*Config, error) {
-	cfg := &Config{}
+type Option func(*Config)
 
-	flag.StringVar(&cfg.RunAddress, "a", "localhost:8080", "HTTP server address")
-	flag.StringVar(&cfg.DatabaseURI, "d", "", "PostgreSQL connection URI")
-	flag.StringVar(&cfg.AccrualSystemAddress, "r", "", "Accrual system address")
-	flag.StringVar(&cfg.JWTSecret, "j", "", "JWT secret key")
+func defaultConfig() *Config {
+	return &Config{
+		RunAddress:            "localhost:8080",
+		JWTSecret:             "gophermart-default-secret-key",
+		AccrualWorkers:        5,
+		AccrualMaxConcurrency: 5,
+		AccrualCheckInterval:  1 * time.Second,
+	}
+}
+
+func New(opts ...Option) *Config {
+	cfg := defaultConfig()
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	return cfg
+}
+
+func WithRunAddress(addr string) Option {
+	return func(c *Config) {
+		if addr != "" {
+			c.RunAddress = addr
+		}
+	}
+}
+
+func WithDatabaseURI(uri string) Option {
+	return func(c *Config) {
+		c.DatabaseURI = uri
+	}
+}
+
+func WithAccrualSystemAddress(addr string) Option {
+	return func(c *Config) {
+		c.AccrualSystemAddress = addr
+	}
+}
+
+func WithJWTSecret(secret string) Option {
+	return func(c *Config) {
+		if secret != "" {
+			c.JWTSecret = secret
+		}
+	}
+}
+
+func WithAccrualWorkers(n int) Option {
+	return func(c *Config) {
+		if n > 0 {
+			c.AccrualWorkers = n
+		}
+	}
+}
+
+func WithAccrualMaxConcurrency(n int) Option {
+	return func(c *Config) {
+		if n > 0 {
+			c.AccrualMaxConcurrency = n
+		}
+	}
+}
+
+func WithAccrualCheckInterval(d time.Duration) Option {
+	return func(c *Config) {
+		if d > 0 {
+			c.AccrualCheckInterval = d
+		}
+	}
+}
+
+func Load() (*Config, error) {
+	var runAddress, databaseURI, accrualAddr, jwtSecret string
+
+	flag.StringVar(&runAddress, "a", "", "HTTP server address")
+	flag.StringVar(&databaseURI, "d", "", "PostgreSQL connection URI")
+	flag.StringVar(&accrualAddr, "r", "", "Accrual system address")
+	flag.StringVar(&jwtSecret, "j", "", "JWT secret key")
 
 	flag.Parse()
 
 	if envAddr := os.Getenv("RUN_ADDRESS"); envAddr != "" {
-		cfg.RunAddress = envAddr
+		runAddress = envAddr
 	}
-
 	if envDB := os.Getenv("DATABASE_URI"); envDB != "" {
-		cfg.DatabaseURI = envDB
+		databaseURI = envDB
 	}
-
 	if envAccrual := os.Getenv("ACCRUAL_SYSTEM_ADDRESS"); envAccrual != "" {
-		cfg.AccrualSystemAddress = envAccrual
+		accrualAddr = envAccrual
 	}
-
 	if envSecret := os.Getenv("JWT_SECRET"); envSecret != "" {
-		cfg.JWTSecret = envSecret
+		jwtSecret = envSecret
 	}
 
-	if cfg.JWTSecret == "" {
-		cfg.JWTSecret = "gophermart-default-secret-key"
+	opts := []Option{
+		WithRunAddress(runAddress),
+		WithDatabaseURI(databaseURI),
+		WithAccrualSystemAddress(accrualAddr),
+		WithJWTSecret(jwtSecret),
 	}
 
-	cfg.AccrualWorkers = 5
 	if envWorkers := os.Getenv("ACCRUAL_WORKERS"); envWorkers != "" {
 		if val, err := strconv.Atoi(envWorkers); err == nil {
-			cfg.AccrualWorkers = val
+			opts = append(opts, WithAccrualWorkers(val))
 		}
 	}
-
-	cfg.AccrualMaxConcurrency = 5
 	if envMaxConcurrency := os.Getenv("ACCRUAL_MAX_CONCURRENCY"); envMaxConcurrency != "" {
 		if val, err := strconv.Atoi(envMaxConcurrency); err == nil {
-			cfg.AccrualMaxConcurrency = val
+			opts = append(opts, WithAccrualMaxConcurrency(val))
 		}
 	}
-
-	cfg.AccrualCheckInterval = 1 * time.Second
 	if envInterval := os.Getenv("ACCRUAL_CHECK_INTERVAL"); envInterval != "" {
 		if val, err := time.ParseDuration(envInterval); err == nil {
-			cfg.AccrualCheckInterval = val
+			opts = append(opts, WithAccrualCheckInterval(val))
 		}
 	}
 
-	return cfg, nil
+	return New(opts...), nil
 }
